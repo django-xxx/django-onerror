463c2723de843f80a7e875370da17818872fec09
Brightcells <brightcells@gmail.com>
2018-09-13 11:17:53 +0800

    :art: Support switch

--- django_onerror/admin.py
+++ django_onerror/admin.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 
+from django.conf import settings
 from django.contrib import admin
 from django_admin import DeleteOnlyModelAdmin
 from django_onerror.models import OnerrorReportInfo
 
 
 class OnerrorReportInfoAdmin(DeleteOnlyModelAdmin, admin.ModelAdmin):
-    list_display = ('lineNo', 'columnNo', 'scriptURI', 'errorMessage', 'stack', 'created_at', 'updated_at')
+    list_display = ('lineNo', 'columnNo', 'scriptURI', 'errorMessage', 'stack', 'status', 'created_at', 'updated_at')
     search_fields = ('lineNo', 'columnNo', 'scriptURI', 'errorMessage', 'stack')
 
 
-admin.site.register(OnerrorReportInfo, OnerrorReportInfoAdmin)
+if not hasattr(settings, 'DJANGO_ONERROR_ADMIN_SITE_REGISTER') or settings.DJANGO_ONERROR_ADMIN_SITE_REGISTER:
+    admin.site.register(OnerrorReportInfo, OnerrorReportInfoAdmin)
--- django_onerror/views.py
+++ django_onerror/views.py
@@ -1,18 +1,22 @@
+# -*- coding: utf-8 -*-
+
 import json
 
+from django.conf import settings
 from django_onerror.models import OnerrorReportInfo
 from django_response import response
 
 
 def err_report(request):
-    payload = json.loads(request.body)
+    if not hasattr(settings, 'DJANGO_ONERROR_ACCEPT_REPORT') or settings.DJANGO_ONERROR_ACCEPT_REPORT:
+        payload = json.loads(request.body)
 
-    OnerrorReportInfo.objects.create(
-        lineNo=payload.get('lineNo', -1),
-        columnNo=payload.get('columnNo', -1),
-        scriptURI=payload.get('scriptURI', ''),
-        errorMessage=payload.get('errorMessage', ''),
-        stack=payload.get('stack', ''),
-    )
+        OnerrorReportInfo.objects.create(
+            lineNo=payload.get('lineNo', -1),
+            columnNo=payload.get('columnNo', -1),
+            scriptURI=payload.get('scriptURI', ''),
+            errorMessage=payload.get('errorMessage', ''),
+            stack=payload.get('stack', ''),
+        )
 
     return response()
